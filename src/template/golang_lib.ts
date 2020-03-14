f94137e5e30bbadaae4816b3ccd56c4121270ccc
joy <joy.lai@zg.com>
2020-03-14 11:43:05 +0800

    fix golang

--- src/template/golang_lib.ts
+++ src/template/golang_lib.ts
@@ -7,25 +7,23 @@ export default class GolangLib implements InterfaceTemplate {
   }
 
   do(shellHelper: ShellHelper, projectName: string, desc: string, repoUrl: string, otherArgs: string[]): void {
-    if (!repoUrl) {
-      throw new Error(`repo url is required`)
-    }
-    let packageName: string = ``
-    const atPos = repoUrl.indexOf(`@`)
-    if (atPos !== -1) {
-      const colonPos = repoUrl.indexOf(`:`)
-      packageName = repoUrl.substring(atPos + 1, colonPos) + `/` + repoUrl.substring(colonPos + 1, repoUrl.indexOf(`.git`))
-    } else {
-      packageName = repoUrl.substring(repoUrl.indexOf(`//`) + 2, repoUrl.indexOf(`.git`))
-    }
-    if (!packageName) {
-      throw new Error(`packageName parse error`)
-    }
-    
     shellHelper.execSync(`git clone ${this.getTemplateRepoUrl()} --single-branch -v -b master --depth 1 ${projectName}`)
     shellHelper.cd(projectName)
     shellHelper.execSync(`rm -rf .git`)
-    shellHelper.execSync(`cat go.mod | sed "s/create_golang_lib_template/${packageName.replaceAll_(`/`, "\\\/")}/g" > temp && rm -rf go.mod && mv temp go.mod`)
-  }
+    if (repoUrl) {
+      let packageName: string = ``
+      const atPos = repoUrl.indexOf(`@`)
+      if (atPos !== -1) {
+        const colonPos = repoUrl.indexOf(`:`)
+        packageName = repoUrl.substring(atPos + 1, colonPos) + `/` + repoUrl.substring(colonPos + 1, repoUrl.indexOf(`.git`))
+      } else {
+        packageName = repoUrl.substring(repoUrl.indexOf(`//`) + 2, repoUrl.indexOf(`.git`))
+      }
+      if (!packageName) {
+        throw new Error(`packageName parse error`)
+      }
 
+      shellHelper.execSync(`cat go.mod | sed "s/create_golang_lib_template/${packageName.replaceAll_(`/`, "\\\/")}/g" > temp && rm -rf go.mod && mv temp go.mod`)
+    }
+  }
 }
