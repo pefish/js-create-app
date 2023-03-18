f6e928171c3662cc59fd15b2959156bb07e45897
pefish <pefish@qq.com>
2023-03-18 14:59:41 +0800

    Update solidity_dapp.ts

--- src/template/solidity_dapp.ts
+++ src/template/solidity_dapp.ts
@@ -3,7 +3,7 @@ import ShellHelper from '@pefish/js-helper-shell'
 
 export default class SolidityDApp implements InterfaceTemplate {
   do(shellHelper: ShellHelper, projectName: string, desc: string, repoUrl: string, otherArgs: string[]): void {
-    shellHelper.execSync(`git clone ${this.getTemplateRepoUrl()} --single-branch -v -b master --depth 1 ${projectName}`)
+    shellHelper.execSync(`git clone ${this.getTemplateRepoUrl()} --single-branch -v -b main --depth 1 ${projectName}`)
     shellHelper.cd(projectName)
     shellHelper.execSync(`rm -rf .git`)
     shellHelper.execSync(`yarn`)
